16709d28e225f35207a8dc5189adce8eabe55983
CECNL <90022378+CECNL@users.noreply.github.com>
2023-10-23 15:17:24 +0800

    Add files via upload

---
+++ utils/getDataLoader.py
@@ -0,0 +1,137 @@
+import numpy as np
+import torch
+from torch import from_numpy as np2TT
+from torch.utils.data import DataLoader, Dataset
+import random
+
+def get_dataloader_4_cross_sub(sub_list, data, truth, ts_sub_idx, args):
+    
+    # use the subject right after the testing subject as validation subject
+    if ts_sub_idx == len(sub_list)-1:
+        val_sub_idx = 2
+    else:
+        val_sub_idx = ts_sub_idx + 1
+    
+    val_data = np.array(data[sub_list[val_sub_idx]][0], dtype=np.float32)
+    val_truth = truth[sub_list[val_sub_idx]][0].astype('float32')
+    val_session_bound = np.tile([0, val_data.shape[0]-1], (val_data.shape[0], 1))
+
+    # Gathering cross-subject training data
+    train_data = []
+    train_truth = []
+    tr_session_bound = []
+    low = 0
+    # Iterate all subject and skip the subjects serving as val or testing subject
+    # We also need to record the boundary of each session so that we can select other trials within the session to form the pair
+    for tr_sub_idx in range(len(sub_list)):
+        if tr_sub_idx == ts_sub_idx or tr_sub_idx == val_sub_idx:
+            continue
+        for sess_idx in range(len(data[sub_list[tr_sub_idx]])):
+            train_data = train_data + data[sub_list[tr_sub_idx]][sess_idx]
+            train_truth.append(truth[sub_list[tr_sub_idx]][sess_idx].astype('float32'))
+            data_len = len(data[sub_list[tr_sub_idx]][sess_idx])
+            tr_session_bound.append(np.tile([low, low + data_len - 1], (data_len, 1)))
+            low += data_len
+
+    train_data = np.array(train_data, dtype=np.float32) # (#total training trial, #window, #channel, #timepoint)
+    train_truth = np.concatenate(train_truth, 0) # (#total training trial, )
+    tr_session_bound = np.concatenate(tr_session_bound, 0) # (#total training trial, 2)
+
+    # Wrap up as dataloader
+    train_dl = get_dataloader(train_data, train_truth, tr_session_bound, 'train', 'static', **args)
+    val_dl = get_dataloader(val_data, val_truth, val_session_bound, 'test', 'static', **args)
+
+    del train_data, train_truth, val_data, val_truth, tr_session_bound, val_session_bound
+
+    return train_dl, val_dl
+
+
+def get_dataloader(data, truth, sess_bound=None, mode = 'train', pairing_mode='dynamic', batch_size=20, shuffle=True, pairing=1, method='siamese', **kwargs):
+    
+    # Convert numpy array to tensor
+    data = np2TT(data)
+    truth = np2TT(truth)
+    print("x train: ", type(data), data.size())
+
+    # Form dataset based on the method (Siamese->pair; Others->normal)
+    if method == 'siamese':
+        dataset = pair_Dataset(data, truth, sess_bound, pairing_mode, pairing)
+    else:
+        dataset = normal_dataset(data, truth)
+    
+    if mode == 'train':
+        dl = DataLoader(
+            dataset = dataset,
+            batch_size = batch_size,
+            shuffle = shuffle,
+            num_workers = 4,
+            pin_memory=True,
+        )
+    elif mode == 'test':
+        dl = DataLoader(
+            dataset = dataset,
+            batch_size = len(dataset),
+            shuffle = False,
+            num_workers = 4,
+            pin_memory=True
+        )
+    else:
+        raise ValueError('Invalid mode!! (dynamic or static)')
+    
+    return dl
+
+class pair_Dataset(Dataset):
+    def __init__(self, data, truth, session, mode, pairing=1):
+        self.data = data
+        self.truth = truth
+        self.session_bound = session
+        self.mode = mode
+        self.pairing = pairing
+
+    def __len__(self):
+        return len(self.data)		
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.truth[index].view(1, 1)
+
+        # form the returned data based on the mode
+        # dynamic baseline training (randomly samlpe trail within the same session as dynamic baseline)
+        # static baseline inference (only select a fixed baseline trial which is the begining of the session)
+        if self.mode=='dynamic':
+            population = range(self.session_bound[index][0], self.session_bound[index][1]) # set the boundary of a session (to ensure that we won't select the trial out of the current session)
+            sample_idx = random.sample(population, self.pairing) # randomly sample trials within the session
+        elif self.mode =='static':
+            sample_idx = [self.session_bound[index][0]] # only set the default baseline trial
+        else:
+            raise ValueError('Invalid mode')
+
+        pair = []
+        label = []
+        # pair the current trial with sampled trial(dynamic or static baseline)
+        for i in range(len(sample_idx)):
+            sample_data = self.data[sample_idx[i]]
+            sample_truth = self.truth[sample_idx[i]].view(1,1)
+
+            pair.append(torch.cat((sample_data, x), 0))
+            label.append(torch.cat((sample_truth, y), 0))
+
+        input_pair = torch.stack(pair)
+        label_pair = torch.stack(label)
+
+        return input_pair, label_pair
+
+class normal_dataset(Dataset):
+    def __init__(self, data, truth):
+        self.data = data
+        self.truth = truth
+
+    def __len__(self):
+        return len(self.data)       
+
+    def __getitem__(self, index):
+        x = self.data[index]
+        y = self.truth[index].view(-1)
+
+        return x, y
+
---
+++ utils/train_test.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from math import sqrt
+from tqdm import tqdm
+
+class grad_accumulator:
+    def __init__(self, EEG_ch, num_window):
+
+        self.num_win = num_window
+        self.grad_acc = {}
+        self.grad_acc["drowsy"] = torch.zeros((self.num_win, EEG_ch, 750))
+        self.grad_acc["all"] = torch.zeros((self.num_win, EEG_ch, 750))
+        self.grad_acc["alert"] = torch.zeros((self.num_win, EEG_ch, 750))
+
+    def update(self, grad):
+        self.grad_acc["all"] += torch.sum(grad[:, self.num_win:, :].cpu(), 0)
+
+class CCCLoss(nn.Module):
+    def __init__(self):
+        super(CCCLoss, self).__init__()
+        self.mean = torch.mean
+        self.var = torch.var
+        self.sum = torch.sum
+        self.sqrt = torch.sqrt
+        self.std = torch.std
+
+    def forward(self, prediction, ground_truth):
+        mean_gt = self.mean (ground_truth, 0)
+        mean_pred = self.mean (prediction, 0)
+        var_gt = self.var (ground_truth, 0)
+        var_pred = self.var (prediction, 0)
+        v_pred = prediction - mean_pred
+        v_gt = ground_truth - mean_gt
+        cor = self.sum (v_pred * v_gt) / (self.sqrt(self.sum(v_pred ** 2)) * self.sqrt(self.sum(v_gt ** 2)))
+        sd_gt = self.std(ground_truth)
+        sd_pred = self.std(prediction)
+        numerator=2*cor*sd_gt*sd_pred
+        denominator=var_gt+var_pred+(mean_gt-mean_pred)**2
+        ccc = numerator/denominator
+        return ccc
+
+"""# Training setup"""
+
+def train_model(model, train_dl, val_dl, save_path=None, model_name=None, epochs=50, **cfg):
+    
+    optimizer = getattr(optim, cfg['optimizer'])(model.parameters(), lr=cfg['lr'], weight_decay=cfg['weight_decay'])
+    criterion = nn.MSELoss(reduction='mean')
+    # criterion = CCCLoss()
+    #lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.99)
+    record = {'train loss': [], 'val loss':[], 'val cc':[]}
+    total_loss = 0
+    mini = 1e8
+    obt_grad = True if cfg['save_grad'] else False
+    grad = grad_accumulator(cfg['EEG_ch'], cfg['num_window'])
+    
+    print("==========Start training==========")
+    for epoch in range(epochs):
+        model.train()
+        print(f"[{epoch+1}/{epochs}]")
+
+        with tqdm(train_dl, unit="batch") as tepoch:
+            for b, (x_train, y_train) in enumerate(tepoch):
+                
+                optimizer.zero_grad()
+                
+                ## If we use Siamese architecture
+                ## we need to merge the multiple pairs into mini-batch
+                ## The ground truth become Delta DI (current DI - baseline DI)
+                if cfg["method"] == 'siamese':
+                    x_train, y_train = torch.flatten(x_train, 0, 1), torch.flatten(y_train, 0, 1)
+                    y_train = y_train[:, 1] - y_train[:, 0] # index 0 in the 2nd dim serve as baseline
+
+                x_train, y_train = x_train.to(cfg['device']), y_train.to(cfg['device'])
+                x_train.requires_grad = obt_grad
+
+                _, pred = model(x_train)
+                loss = criterion(pred, y_train)
+                total_loss += loss.detach().cpu().item()
+
+                loss.backward(retain_graph=obt_grad)
+
+                if(obt_grad):
+                    grad.update(x_train.grad)
+
+                optimizer.step()
+                
+                tepoch.set_postfix(loss = total_loss/(b+1))
+                tepoch.update(1)
+
+        del x_train, y_train
+
+        ### Validation
+        val_loss, rmse, cc = val_model(model, val_dl, cfg["method"], cfg['device'])
+        record["train loss"].append(total_loss/len(train_dl.dataset))
+        record["val loss"].append(val_loss)
+        record["val cc"].append(cc)
+
+        print(f"val loss-> {val_loss} rmse -> {rmse} cc -> {cc}")
+        matrice = 1.0* rmse + 0.0*(1-cc)
+        if matrice < mini:
+            mini = matrice
+            model_save_path = f'{save_path}{model_name}_model.pt' # Use test subject to name the model
+            torch.save(model.state_dict(), model_save_path)
+        
+        total_loss = 0
+
+    torch.cuda.empty_cache()
+    return record, grad.grad_acc
+
+def val_model(model, test_dl, method='siamese', device='cpu'):
+    criterion_mse = nn.MSELoss(reduction='mean')
+
+    with torch.no_grad():
+        model.eval()
+        for x_test, y_test in test_dl:
+
+            if method == 'siamese':
+                x_test, y_test = torch.flatten(x_test, 0, 1), torch.flatten(y_test, 0, 1)
+                y_test = y_test[:,0] - y_test[:,1]
+
+            x_test, y_test = x_test.to(device), y_test.to(device)
+
+            _, pred = model(x_test)
+            
+            mse = criterion_mse(pred, y_test)
+            rmse = sqrt(mse)
+            cc = np.corrcoef(y_test.cpu().detach().numpy().reshape(-1), pred.cpu().detach().numpy().reshape(-1))[0, 1]
+
+        del x_test, y_test
+
+    return mse, rmse, cc
+
+def test_model(model, test_dl, method='siamese', device='cpu'):
+    
+    model.eval()
+    with torch.no_grad():
+        for x_test, y_test in test_dl:
+
+            if method == 'siamese':
+                x_test, y_test = torch.flatten(x_test, 0, 1), torch.flatten(y_test, 0, 1)
+                y_test = y_test[:,0] - y_test[:,1]
+            
+            x_test, y_test = x_test.to(device), y_test.to(device)
+
+            latent, delta_di = model(x_test)
+
+        del x_test, y_test
+
+    return latent, delta_di
