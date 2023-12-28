6c102827c45ba83a74bf8e86c4463e64c18fa9ce
CECNL <90022378+CECNL@users.noreply.github.com>
2023-12-28 16:02:21 +0800

    remove redundant function

--- utils/train_test.py
+++ utils/train_test.py
@@ -17,30 +17,6 @@ class grad_accumulator:
     def update(self, grad):
         self.grad_acc["all"] += torch.sum(grad[:, self.num_win:, :].cpu(), 0)
 
-class CCCLoss(nn.Module):
-    def __init__(self):
-        super(CCCLoss, self).__init__()
-        self.mean = torch.mean
-        self.var = torch.var
-        self.sum = torch.sum
-        self.sqrt = torch.sqrt
-        self.std = torch.std
-
-    def forward(self, prediction, ground_truth):
-        mean_gt = self.mean (ground_truth, 0)
-        mean_pred = self.mean (prediction, 0)
-        var_gt = self.var (ground_truth, 0)
-        var_pred = self.var (prediction, 0)
-        v_pred = prediction - mean_pred
-        v_gt = ground_truth - mean_gt
-        cor = self.sum (v_pred * v_gt) / (self.sqrt(self.sum(v_pred ** 2)) * self.sqrt(self.sum(v_gt ** 2)))
-        sd_gt = self.std(ground_truth)
-        sd_pred = self.std(prediction)
-        numerator=2*cor*sd_gt*sd_pred
-        denominator=var_gt+var_pred+(mean_gt-mean_pred)**2
-        ccc = numerator/denominator
-        return ccc
-
 """# Training setup"""
 
 def train_model(model, train_dl, val_dl, save_path=None, model_name=None, epochs=50, **cfg):
